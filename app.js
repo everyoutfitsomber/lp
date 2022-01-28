32958d07bf6b58c007574ed9bc5ce771f41c305a
Hamid <hamidtahir2000@gmail.com>
2022-01-28 17:37:50 +0500

    Modal Fixes + Chat widget + Scroll bar

--- app.js
+++ app.js
@@ -17,3 +17,20 @@ window.onload = function () {
     }
   });
 };
+
+// Live Chat
+// Start of Tawk.to Script
+
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/61e5d921b84f7301d32b82b8/1fpktgb2m";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
+
+//End of Tawk.to Script
